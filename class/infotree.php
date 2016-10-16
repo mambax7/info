cc67b39f2c99a837debfce6c57cd482864d0eed4
mambax7 <mambax7@gmail.com>
2016-10-16 04:42:11 -0400

    Argument PHPDoc missing

--- class/infotree.php
+++ class/infotree.php
@@ -329,9 +329,10 @@ class InfoTree
     /**
      * Enter description here...
      *
-     * @param int $sel_id
+     * @param int    $sel_id
      * @param string $order
-     * @param array $parray
+     * @param array  $parray
+     * @param null   $extra
      * @return array
      */
     public function getAllChild($sel_id = 0, $order = '', $parray = array(), $extra=null)
@@ -354,13 +355,15 @@ class InfoTree
         }
         return $parray;
     }
+
     /**
      * Enter description here...
      *
-     * @param int $sel_id
+     * @param int    $sel_id
      * @param string $order
-     * @param array $parray
+     * @param array  $parray
      * @param string $r_prefix
+     * @param null   $extra
      * @return array
      */
     public function getChildTreeArray($sel_id = 0, $order = '', $parray = array(), $r_prefix = '', $extra = null)
