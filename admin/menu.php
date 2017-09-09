98c1fbcab81be8d230cac36034f19e87bca42a13
mambax7 <mambax7@gmail.com>
2017-09-09 11:07:39 -0400

    Menu standardization

--- admin/menu.php
+++ admin/menu.php
@@ -25,7 +25,6 @@ if (false !== ($moduleHelper = Xmf\Module\Helper::getHelper($moduleDirName))) {
     $moduleHelper = Xmf\Module\Helper::getHelper('system');
 }
 
-
 $pathIcon32 = \Xmf\Module\Admin::menuIconPath('');
 //$pathModIcon32 = $moduleHelper->getModule()->getInfo('modicons32');
 
@@ -36,30 +35,38 @@ require_once dirname(__DIR__) . '/class/info.php';
 $infowaitHandler = new InfoInfoHandler($GLOBALS['xoopsDB'], $moduleDirName . '_bak');
 $wait_site       = $infowaitHandler->getCount();
 
-$adminmenu               = [];
-$i                       = 0;
-'title' =>  _MI_INFO_INDEX,
-'link' =>  'admin/index.php',
-$adminmenu[$i++]['icon'] = $pathIcon32 . '/home.png';
+$adminmenu[] = [
+    'title' => _MI_INFO_INDEX,
+    'link'  => 'admin/index.php',
+    'icon'  => $pathIcon32 . '/home.png',
+];
 
-'title' =>  _MI_INFO_ADMENU2,
-'link' =>  'admin/admin_categorie.php',
-$adminmenu[$i++]['icon'] = $pathIcon32 . '/category.png';
+$adminmenu[] = [
+    'title' => _MI_INFO_ADMENU2,
+    'link'  => 'admin/admin_categorie.php',
+    'icon'  => $pathIcon32 . '/category.png',
+];
 
-'title' =>  _MI_INFO_ADMENU3,
-'link' =>  'admin/admin_seiten.php',
-$adminmenu[$i++]['icon'] = $pathIcon32 . '/view_detailed.png';
+$adminmenu[] = [
+    'title' => _MI_INFO_ADMENU3,
+    'link'  => 'admin/admin_seiten.php',
+    'icon'  => $pathIcon32 . '/view_detailed.png',
+];
 
-'title' =>  '(' . $wait_site . ') ' . _MI_INFO_ADMENU5,
-'link' => approved',
-$adminmenu[$i++]['icon'] = $pathIcon32 . '/manage.png';
+$adminmenu[] = [
+    'title' => '(' . $wait_site . ') ' . _MI_INFO_ADMENU5,
+    'link'  => 'admin/admin_seiten.php?op=approved',
+    'icon'  => $pathIcon32 . '/manage.png',
+];
 
-'title' =>  _MI_INFO_ADMENU4,
-'link' =>  'admin/admin_permission.php',
-$adminmenu[$i++]['icon'] = $pathIcon32 . '/permissions.png';
+$adminmenu[] = [
+    'title' => _MI_INFO_ADMENU4,
+    'link'  => 'admin/admin_permission.php',
+    'icon'  => $pathIcon32 . '/permissions.png',
+];
 
-'title' =>  _MI_INFO_ADMENU_ABOUT,
-'link' =>  'admin/about.php',
-$adminmenu[$i++]['icon'] = $pathIcon32 . '/about.png';
-
-unset($i);
+$adminmenu[] = [
+    'title' => _MI_INFO_ADMENU_ABOUT,
+    'link'  => 'admin/about.php',
+    'icon'  => $pathIcon32 . '/about.png',
+];
