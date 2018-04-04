7ae72137931b72e8e7c713b93b8f2152c6569f90
mambax7 <mambax7@gmail.com>
2018-04-04 08:13:01 -0400

    updates
    Signed-off-by: mambax7 <mambax7@gmail.com>

--- include/onupdate.php
+++ include/onupdate.php
@@ -17,6 +17,8 @@
  * @author       XOOPS Development Team
  */
 
+use XoopsModules\Info;
+
 if ((!defined('XOOPS_ROOT_PATH')) || !($GLOBALS['xoopsUser'] instanceof \XoopsUser)
     || !$GLOBALS['xoopsUser']->IsAdmin()
 ) {
@@ -45,10 +47,10 @@ function tableExists($tablename)
 function xoops_module_pre_update_info(\XoopsModule $module)
 {
     $moduleDirName = basename(dirname(__DIR__));
-    /** @var info\Helper $helper */
-    /** @var info\Utility $utility */
-    $helper       = info\Helper::getInstance();
-    $utility      = new info\Utility();
+    /** @var Info\Helper $helper */
+    /** @var Info\Utility $utility */
+    $helper       = Info\Helper::getInstance();
+    $utility      = new Info\Utility();
 
     $xoopsSuccess = $utility::checkVerXoops($module);
     $phpSuccess   = $utility::checkVerPhp($module);
@@ -69,27 +71,14 @@ function xoops_module_update_info(\XoopsModule $module, $previousVersion = null)
     $moduleDirName = basename(dirname(__DIR__));
     $capsDirName   = strtoupper($moduleDirName);
 
-    /** @var info\Helper $helper */
-    /** @var info\Utility $utility */
-    /** @var info\Configurator $configurator */
-    $helper  = info\Helper::getInstance();
-    $utility = new info\Utility();
-    $configurator = new info\Configurator();
+    /** @var Info\Helper $helper */
+    /** @var Info\Utility $utility */
+    /** @var Info\Common\Configurator $configurator */
+    $helper  = Info\Helper::getInstance();
+    $utility = new Info\Utility();
+    $configurator = new Info\Common\Configurator();
 
     if ($previousVersion < 240) {
-
-        //rename column EXAMPLE
-        $tables     = new Tables();
-        $table      = 'infox_categories';
-        $column     = 'ordre';
-        $newName    = 'order';
-        $attributes = "INT(5) NOT NULL DEFAULT '0'";
-        if ($tables->useTable($table)) {
-            $tables->alterColumn($table, $column, $attributes, $newName);
-            if (!$tables->executeQueue()) {
-                echo '<br>' . _AM_XXXXX_UPGRADEFAILED0 . ' ' . $migrate->getLastError();
-            }
-        }
 
         //delete old HTML templates
         if (count($configurator->templateFolders) > 0) {
@@ -136,7 +125,7 @@ function xoops_module_update_info(\XoopsModule $module, $previousVersion = null)
         if (count($configurator->uploadFolders) > 0) {
             //    foreach (array_keys($GLOBALS['uploadFolders']) as $i) {
             foreach (array_keys($configurator->uploadFolders) as $i) {
-                $utilityClass::createFolder($configurator->uploadFolders[$i]);
+                $utility::createFolder($configurator->uploadFolders[$i]);
             }
         }
 
@@ -145,7 +134,7 @@ function xoops_module_update_info(\XoopsModule $module, $previousVersion = null)
             $file = __DIR__ . '/../assets/images/blank.png';
             foreach (array_keys($configurator->copyBlankFiles) as $i) {
                 $dest = $configurator->copyBlankFiles[$i] . '/blank.png';
-                $utilityClass::copyFile($file, $dest);
+                $utility::copyFile($file, $dest);
             }
         }
 
@@ -153,9 +142,9 @@ function xoops_module_update_info(\XoopsModule $module, $previousVersion = null)
         $sql = 'DELETE FROM ' . $GLOBALS['xoopsDB']->prefix('tplfile') . " WHERE `tpl_module` = '" . $module->getVar('dirname', 'n') . '\' AND `tpl_file` LIKE \'%.html%\'';
         $GLOBALS['xoopsDB']->queryF($sql);
 
-        /** @var XoopsGroupPermHandler $gpermHandler */
-        $gpermHandler = xoops_getHandler('groupperm');
-        return $gpermHandler->deleteByModule($module->getVar('mid'), 'item_read');
+        /** @var XoopsGroupPermHandler $grouppermHandler */
+        $grouppermHandler = xoops_getHandler('groupperm');
+        return $grouppermHandler->deleteByModule($module->getVar('mid'), 'item_read');
     }
     return true;
 }
